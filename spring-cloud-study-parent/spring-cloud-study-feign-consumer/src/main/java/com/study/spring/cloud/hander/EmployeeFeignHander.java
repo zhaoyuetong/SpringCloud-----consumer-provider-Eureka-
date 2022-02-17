0d04b1f3b5640245ee2752a585557b20cbf25466
zyt_glb <453542525@qq.com>
2022-02-17 18:48:27 +0800

    SpringCloud学习心得--- SpringCloud学习心得---Hystrix熔断降级机制-v1003

--- spring-cloud-study-parent/spring-cloud-study-feign-consumer/src/main/java/com/study/spring/cloud/hander/EmployeeFeignHander.java
+++ spring-cloud-study-parent/spring-cloud-study-feign-consumer/src/main/java/com/study/spring/cloud/hander/EmployeeFeignHander.java
@@ -3,22 +3,33 @@ package com.study.spring.cloud.hander;
 
 import com.study.spring.cloud.api.EmployeeRemoteService;
 import com.study.spring.cloud.entity.Employee;
+import com.study.spring.cloud.util.ResultEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class EmployeeFeignHander {
+    // 装配调用远程微服务的接口，后面向调用本地方法一样直接使用
     @Autowired
     private EmployeeRemoteService employeeRemoteService;
+
+    @RequestMapping("/feign/consumer/test/fallback")
+    public ResultEntity<Employee> testFallBack(@RequestParam("signal") String signal) {
+        return employeeRemoteService.getEmpWithCircuitBreaker(signal);
+    }
+
     @RequestMapping("/feign/consumer/get/emp")
     public Employee getEmployeeRemote() {
         return employeeRemoteService.getEmployeeRemote();
     }
-    @RequestMapping("/feign/consumer/get/emp/id")
-    public Employee getEmployeeById(@RequestParam("id") Integer id){
-       return employeeRemoteService.getEmployeeById(id);
-    }
 
+    @RequestMapping("/feign/consumer/search")
+    public List<Employee> getEmpListRemote(String keyword) {
+
+        return employeeRemoteService.getEmpListRemote(keyword);
+    }
 }
--- spring-cloud-study-parent/spring-cloud-study-provider/src/main/java/com/study/spring/cloud/hander/EmployeeHander.java
+++ spring-cloud-study-parent/spring-cloud-study-provider/src/main/java/com/study/spring/cloud/hander/EmployeeHander.java
@@ -1,22 +1,72 @@
 package com.study.spring.cloud.hander;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.study.spring.cloud.entity.Employee;
+import com.study.spring.cloud.util.ResultEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class EmployeeHander {
-    @RequestMapping("/provider/get/employee/remote")
-    public Employee getEmployeeRemote(HttpServletRequest request) {
-        int port = request.getServerPort();
-        return new Employee(555, "tom555"+port, 555.55);
+
+    private Logger logger = LoggerFactory.getLogger(EmployeeHander.class);
+
+    // @HystrixCommand注解指定当前方法出问题时调用的备份方法（使用fallbackMethod属性指定）
+    @HystrixCommand(fallbackMethod = "getEmpWithCircuitBreakerBackup")
+    @RequestMapping("/provider/get/emp/with/circuit/breaker")
+    public ResultEntity<Employee> getEmpWithCircuitBreaker(@RequestParam("signal") String signal) throws InterruptedException {
+
+        if("quick-bang".equals(signal)) {
+            throw new RuntimeException();
+        }
+
+        if("slow-bang".equals(signal)) {
+            Thread.sleep(5000);
+        }
+
+        return ResultEntity.successWithData(new Employee(666, "empName666", 666.66));
     }
 
-    @RequestMapping("/provider/get/employee/by/id")
-    public Employee getEmployeeById(@RequestParam("empId") Integer empId) {
-        return new Employee(empId, "tom999-", 999.99);
+    public ResultEntity<Employee> getEmpWithCircuitBreakerBackup(@RequestParam("signal") String signal) {
+
+        String message = "方法执行出现问题，执行断路 signal="+signal;
+
+        return ResultEntity.failed(message);
     }
+
+    @RequestMapping("/provider/get/emp/list/remote")
+    public List<Employee> getEmpListRemote(String keyword) {
+
+        logger.info("keyword="+keyword);
+
+        List<Employee> empList = new ArrayList<>();
+        empList.add(new Employee(33, "empName33", 333.33));
+        empList.add(new Employee(44, "empName44", 444.44));
+        empList.add(new Employee(55, "empName55", 555.55));
+
+        return empList;
+    }
+
+    @RequestMapping("/provider/get/employee/remote")
+    public Employee getEmployeeRemote() {
+
+        return new Employee(555, "tom555 ", 555.55);
+    }
+
+//	@RequestMapping("/provider/get/employee/remote")
+//	public Employee getEmployeeRemote(HttpServletRequest request) {
+//
+//		// 获取当前服务的端口号
+//		int serverPort = request.getServerPort();
+//
+//		return new Employee(555, "tom555 "+serverPort, 555.55);
+//	}
+
 }
